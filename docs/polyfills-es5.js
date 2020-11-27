b3f63ddc9fb5935fc8633732d430ef096669e985
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-11-27 13:42:23 +0300

    Update footer on login page; Update demo

--- docs/polyfills-es5.js
+++ docs/polyfills-es5.js
@@ -18899,7 +18899,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! E:\Flatlogic\angular-material-admin\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
+    /*! D:\Projects\angular-material-admin\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -18907,7 +18907,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     module.exports = __webpack_require__(
-    /*! E:\Flatlogic\angular-material-admin\src\polyfills.ts */
+    /*! D:\Projects\angular-material-admin\src\polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
